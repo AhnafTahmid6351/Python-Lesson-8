9ab8282f89e363b682e7767bff0fefb2ef1fced3
AhnafTahmid6351 <ahnaf.jarif09@gmail.com>
2026-05-23 14:41:32 +0600

    Add files via upload

---
+++ Afterclass.py
@@ -0,0 +1,7 @@
+Number = int(input("Enter the number:"))
+Power = int(input("Enter the power:"))
+sum = ""
+i = ""
+for i in range(Power):
+    sum = Number ** Power
+print("Sum of", Number, "with",Power,"is:",sum)
